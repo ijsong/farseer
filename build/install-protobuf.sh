4487dc841a8b77dc3649ade4d992c6b0680e8839
Injun Song <ijsong@gmail.com>
2019-08-25 22:41:23 +0900

    fix build script

--- build/install-protobuf.sh
+++ build/install-protobuf.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 wget https://github.com/google/protobuf/archive/v$PROTOBUF_VERSION.tar.gz
 tar -xzvf v$PROTOBUF_VERSION.tar.gz
-cd protobuf-$PROTOBUF_VERSION && ./configure --prefix=/usr && make && sudo make install
+cd protobuf-$PROTOBUF_VERSION && ./autogen.sh && ./configure --prefix=/usr && make && make check && sudo make install
